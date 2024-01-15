8f1cf406ba79f7dc81545ee73e0d56676a190b8c
kimdohoo1102 <kimdohoo1102@gmail.com>
2024-01-15 14:15:54 +0900

    feat : Creat and Add fizzbuzz.py

---
+++ fizzbuzz.py
@@ -0,0 +1,11 @@
+
+def do_fizzbuzz():
+    for i in range (1,15+1):
+        if i%15 == 0:
+            print('fizzbuzz')
+        elif i%5 == 0:
+                print('buzz')
+
+
+if __name__ == '__main__':
+    do_fizzbuzz()
